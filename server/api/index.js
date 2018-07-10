f4f80e59ca1ff7c5d852ee5d97a1d40ee683ca9e
Kait Hoehne <kait.hoehne@gmail.com>
2018-07-09 20:24:13 -0400

    monday night working on model changes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/characters', require('./characters'))
 router.use('/collections', require('./collections'))
+router.use('/stories', require('./stories'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
