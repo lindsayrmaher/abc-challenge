2f83b6d388ba7df3d1a433cde9f720354e462ace
Lindsay Maher <lindsaymah3r@gmail.com>
2018-07-15 21:29:11 -0400

    adjusts files

--- client/components/Routes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Routes = props => {
-  return (
-    <div>I'm the routes component!</div>
-  )
-}
-export default Routes
--- server/api/collections.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { Collection } = require('../db/models')
-module.exports = router;
-
-router.get('/', (req, res, next) => {
-  Collection.findAll()
-    .then(collections => res.json(collections))
-    .catch(next)
-})
