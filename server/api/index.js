10f12858d4b620f45fa651234af93dcb54a58cfc
Lindsay Maher <lindsaymah3r@gmail.com>
2018-07-16 15:41:16 -0400

    sets up endpoints

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,28 @@
 const router = require('express').Router()
+const NewsAPI = require('newsapi')
+const apiKey = process.env.APIKEY
+const newsapi = new NewsAPI(apiKey)
 module.exports = router
 
-// router.use('/characters', require('./characters'))
-// router.use('/collections', require('./collections'))
-// router.use('/stories', require('./stories'))
+router.get('/', (req, res, next) => {
+  newsapi.v2.topHeadlines({
+    language: 'en'
+  })
+    .then(articles => {
+      res.send(articles).status(200)
+    })
+})
+
+router.get('/:category', (req, res, next) => {
+  newsapi.v2.topHeadlines({
+    category: req.params.category,
+    language: 'en'
+  })
+    .then(articles => {
+      res.send(articles).status(200)
+    })
+})
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
