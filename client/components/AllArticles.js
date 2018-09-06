1000f5320ee11e3e2f7378ba5ce3ecb16978bba3
Lindsay Maher <lindsay.r.maher@abc.com>
2018-09-06 17:15:46 -0400

    little changes to compare with diff project

--- client/components/AllArticles.js
+++ client/components/AllArticles.js
@@ -1,71 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Article from './Article'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Article from './Article';
 
 export default class AllArticles extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       articles: [],
       selectedSource: ''
-    }
-    this.sortArticles = this.sortArticles.bind(this)
+    };
+    this.sortArticles = this.sortArticles.bind(this);
   }
 
   componentDidMount() {
-    this.getArticles()
+    this.getArticles();
   }
 
   async getArticles() {
     try {
-      const allArticles = await axios.get('/api/')
-      let articles = allArticles.data.articles
-      articles = this.sortArticles(articles)
-      this.setState({ articles })
-    }
-    catch (error) {
-      console.error('error fetching articles', error)
+      const allArticles = await axios.get('/api/');
+      let articles = allArticles.data.articles;
+      articles = this.sortArticles(articles);
+      this.setState({ articles });
+    } catch (error) {
+      console.error('error fetching articles', error);
     }
   }
 
   async getArticlesBySource() {
     try {
-      const allArticles = await axios.get('/api/:source')
-      let articles = allArticles.data.articles
-      articles = this.sortArticles(articles)
-      this.setState({ articles })
-    }
-    catch (error) {
-      console.error('error fetching articles', error)
+      const allArticles = await axios.get('/api/:source');
+      let articles = allArticles.data.articles;
+      articles = this.sortArticles(articles);
+      this.setState({ articles });
+    } catch (error) {
+      console.error('error fetching articles', error);
     }
   }
 
   sortArticles(articles) {
-    let sortedArticles
+    let sortedArticles;
     if (this.state.articles) {
       sortedArticles = articles.sort((a, b) => {
-        let dateA = new Date(a.publishedAt)
-        let dateB = new Date(b.publishedAt)
-        return dateA < dateB ? 1 : -1
-      })
+        let dateA = new Date(a.publishedAt);
+        let dateB = new Date(b.publishedAt);
+        return dateA < dateB ? 1 : -1;
+      });
     }
-    return sortedArticles
+    return sortedArticles;
   }
 
-
   render() {
-    const articles = this.state.articles
-    console.log(articles)
+    const { articles } = this.state;
+    console.log(articles);
     return (
       <div>
-        {
-          !articles || articles.length === 0 ?
-            <div>No articles to load!</div> :
-            articles.map(article => {
-              return <Article key={article.id} articleInfo={article} />
-            })
-        }
+        {!articles || articles.length === 0 ? (
+          <div>No articles to load!</div>
+        ) : (
+          articles.map(article => (
+            <Article key={article.id} articleInfo={article} />
+          ))
+        )}
       </div>
-    )
+    );
   }
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import history from './history'
-import { Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import App from './app';
+import history from './history';
 
 ReactDOM.render(
   <Router history={history}>
